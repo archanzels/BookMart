b5401a034a187bcc09ebf172fb15e38b6c781349
Ark <archanzels@gmail.com>
2017-02-06 19:23:06 +0545

    Just few updates to make the app look better Added category. Homepage modified. Fiction Fragment Added Buy Page Added

--- app/src/main/java/com/example/bookmart/FictionFragment.java
+++ app/src/main/java/com/example/bookmart/FictionFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * Created by Ark on 12/4/2016.
@@ -18,6 +19,19 @@ public class FictionFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.fragment_fiction, container, false);
+
+        //Get button ID
+        ImageButton img = (ImageButton) myView.findViewById(R.id.fic1);
+        Fragment fragment = new Fragment();
+        final android.app.FragmentManager fragmentManager = getFragmentManager();
+
+
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction().replace(R.id.content_frame, new BuyFragment()).commit();
+            }
+        });
         return myView;
     }
 }
